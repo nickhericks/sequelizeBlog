367210f014a18a0bb45d30940b17aff5dd0bef89
Nick-Hericks <nickhericks@gmail.com>
2019-05-20 13:22:55 +0200

    Add 500 error catch methods

--- routes/articles.js
+++ routes/articles.js
@@ -5,16 +5,24 @@ var Article = require('../models').Article;
 
 /* GET articles listing. */
 router.get('/', function(req, res, next) {
-	Article.findAll({order: [['createdAt', 'DESC']]}).then(function(articles){
-		res.render('articles/index', {articles: articles, title: 'My Awesome Blog' });
-	});
+	Article.findAll({order: [['createdAt', 'DESC']]})
+		.then(function(articles){
+			res.render('articles/index', {articles: articles, title: 'My Awesome Blog' })
+				.catch(function(err){
+					res.send(500);
+				});
+		});
 });
 
 /* POST create article. */
 router.post('/', function(req, res, next) {
-	Article.create(req.body).then(function(article) {
-		res.redirect('/articles/' + article.id);
-	});
+	Article.create(req.body)
+		.then(function(article) {
+			res.redirect('/articles/' + article.id);
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 /* Create a new article form. */
@@ -24,43 +32,71 @@ router.get('/new', function(req, res, next) {
 
 /* Edit article form. */
 router.get('/:id/edit', function(req, res, next){
-	Article.findByPk(req.params.id).then(function(article) {
-		res.render('articles/edit', {article: article, title: 'Edit Article'});
-	});
+	Article.findByPk(req.params.id)
+		.then(function(article) {
+			res.render('articles/edit', {
+				article: article,
+				title: 'Edit Article'
+			});
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 
 /* Delete article form. */
 router.get('/:id/delete', function(req, res, next){
-	Article.findByPk(req.params.id).then(function(article) {
-		res.render('articles/delete', {article: article, title: 'Delete Article'});
-	});
+	Article.findByPk(req.params.id)
+		.then(function(article) {
+			res.render('articles/delete', {
+				article: article,
+				title: 'Delete Article'
+			});
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 
 /* GET individual article. */
 router.get('/:id', function(req, res, next) {
-	Article.findByPk(req.params.id).then(function(article) {
-		res.render('articles/show', { article: article, title: article.title });
-	});
+	Article.findByPk(req.params.id)
+		.then(function(article) {
+			res.render('articles/show', { article: article, title: article.title });
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 /* PUT update article. */
 router.put('/:id', function(req, res, next){
-	Article.findByPk(req.params.id).then(function(article) {
-		return article.update(req.body);
-	}).then(function(article){
-		res.redirect("/articles/" + article.id);    
-	});
+	Article.findByPk(req.params.id)
+		.then(function(article) {
+			return article.update(req.body);
+		})
+		.then(function(article) {
+			res.redirect('/articles/' + article.id);
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 /* DELETE individual article. */
 router.delete('/:id', function(req, res, next){
-	Article.findByPk(req.params.id).then(function(article) {
-		return article.destroy();
-	}).then(function(){
-		res.redirect("/articles");
-	});
+	Article.findByPk(req.params.id)
+		.then(function(article) {
+			return article.destroy();
+		})
+		.then(function() {
+			res.redirect('/articles');
+		})
+		.catch(function(err) {
+			res.send(500);
+		});
 });
 
 
